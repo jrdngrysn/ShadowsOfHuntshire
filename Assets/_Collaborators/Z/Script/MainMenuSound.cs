a1f21767469db6e7bbd14684edf7d150828c0981
zittacheng <zittacheng@gmail.com>
2021-02-27 12:20:14 -0500

    C.C.O.M.M.T.

---
+++ Assets/_Collaborators/Z/Script/MainMenuSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THAN
+{
+    public class MainMenuSound : MonoBehaviour {
+        public static MainMenuSound Main;
+        public AudioSource Source;
+        public AudioReverbFilter Reverb;
+        public bool AlreadyDead;
+
+        public void Awake()
+        {
+            if (!Main || Main.AlreadyDead)
+            {
+                DontDestroyOnLoad(gameObject);
+                Main = this;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GlobalControl.Main && !AlreadyDead)
+                StartCoroutine("Fade");
+        }
+
+        public IEnumerator Fade()
+        {
+            yield return new WaitForSeconds(0.5f);
+            AlreadyDead = true;
+            while (Reverb.decayTime < 15)
+            {
+                Reverb.decayTime += Time.deltaTime * 20;
+                yield return 0;
+            }
+            Source.Pause();
+            Destroy(gameObject, 10);
+        }
+    }
+}
