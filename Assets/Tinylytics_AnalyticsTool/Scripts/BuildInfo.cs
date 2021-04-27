65b759bcaf269e4b3f80c6c932da4d7d86b95ce0
Jordan Grayson <jtg400@nyu.edu>
2021-04-27 15:42:56 -0400

    Tweaked values and playtested to make sure it can get to the end of the game

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/17/2021 1:27:34 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/26/2021 6:40:12 PM";}}
