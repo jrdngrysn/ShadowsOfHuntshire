8b00e225b69d76b20f52f0fa8250a584bad40c44
zittacheng <zittacheng@gmail.com>
2021-05-04 15:28:44 -0400

    Good Ending II

---
+++ Assets/_Collaborators/Z/Script/ChoiceEffect/ChoiceEffect_EndingTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THAN
+{
+    public class ChoiceEffect_EndingTrigger : ChoiceEffect {
+        public string Key;
+
+        public override void Effect(List<Character> Characters, out Event AddEvent)
+        {
+            KeyBase.Main.ChangeKey(Key, 1);
+            base.Effect(Characters, out AddEvent);
+        }
+    }
+}
