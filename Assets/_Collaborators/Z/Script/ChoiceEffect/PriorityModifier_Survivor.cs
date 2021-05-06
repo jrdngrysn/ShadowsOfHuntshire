7d35a11371d151ddc15c2312a507888dc65bc9d6
zittacheng <zittacheng@gmail.com>
2021-05-06 15:02:40 -0400

    Events

--- Assets/_Collaborators/Z/Script/ChoiceEffect/PriorityModifier_Survivor.cs
+++ Assets/_Collaborators/Z/Script/ChoiceEffect/PriorityModifier_Survivor.cs
@@ -10,7 +10,8 @@ namespace THAN
 
         public override int GetPriority(int Value, Character Source, Pair P)
         {
-            if (Character.Find(ActiveName) && Character.Find(ActiveName).Active && (!Character.Find(DeathName) || !Character.Find(DeathName).Active))
+            if (((Character.Find(ActiveName) && Character.Find(ActiveName).Active) || ActiveName == "")
+                && (!Character.Find(DeathName) || !Character.Find(DeathName).Active || DeathName == ""))
                 return Value;
             return -1;
         }
