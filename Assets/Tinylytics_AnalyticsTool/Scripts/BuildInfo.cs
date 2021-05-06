b66bcf1b77e61423ac8c0a2cce18e69ab1e1d3e4
Jordan Grayson <jtg400@nyu.edu>
2021-05-06 14:53:00 -0400

    Updated font of content warning

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/26/2021 6:40:12 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/4/2021 5:18:52 PM";}}
