37d5935293f03291c86f95991272910f1fc5bf91
Jordan Grayson <jtg400@nyu.edu>
2021-05-08 22:48:50 -0400

    Added escape button and tested project settings

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/6/2021 3:19:18 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/6/2021 5:51:17 PM";}}
---
+++ Assets/_Collaborators/General/Script/ESCButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THAN {
+    public class ESCButton : MonoBehaviour
+    {
+        PauseControl pauseControl;
+
+
+        private void Start()
+        {
+            if (GameObject.Find("Pause") != null)
+            {
+                pauseControl = GameObject.Find("Pause").GetComponent<PauseControl>();
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            pauseControl.Active = !pauseControl.Active;
+        }
+    }
+}
