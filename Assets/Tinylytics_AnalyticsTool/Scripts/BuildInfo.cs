40d0bab774c4e0ac561729305fb7f1cca1e52a72
Jordan Grayson <jtg400@nyu.edu>
2021-05-10 20:42:09 -0400

    Adjusted some values

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/9/2021 1:03:20 AM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/10/2021 8:07:04 PM";}}
